45f9c872dd82f521e9302a5bcf4824737db584fe
s.kiram <s.kiram@dm73.net>
2012-12-12 15:37:49 +0000

    BD

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,10 +19,12 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Ens\JobeetBundle\EnsJobeetBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
